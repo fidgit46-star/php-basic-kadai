23c5e11be2e56b24dd5ece35ca924f6109c7c65c
fidgit46-star <fidgit46@gmail.com>
2025-10-03 16:58:28 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+      <?php
+       // 配列に値を代入する
+       $vegetable = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+       // 配列の値を出力する
+       print_r($vegetable);
+      ?>
+   </p>
+</body>
+
+</html>
